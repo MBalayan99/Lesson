b9f488cf60e86d73aedb8e0d973d9843db008d69
Mher2019 <balayan.mher88@gmail.com>
2023-12-15 16:40:41 +0400

    add region

--- CreatEc2_2.sh
+++ CreatEc2_2.sh
@@ -1,49 +1,53 @@
 #!/bin/bash
 
+# Set the region
+region="us-west-1"
+
 # Create VPC
 echo "Creating VPC"
-vpc_id=$(aws ec2 create-vpc --cidr-block 10.0.0.0/16 --query 'Vpc.VpcId' --output text)
+vpc_id=$(aws ec2 create-vpc --cidr-block 10.0.0.0/16 --region $region --query 'Vpc.VpcId' --output text)
 aws ec2 create-tags --resources $vpc_id --tags Key=Name,Value=MyVPC
 
 # Create internet gateway
 echo "Creating Internet Gateway..."
-igw_id=$(aws ec2 create-internet-gateway --query 'InternetGateway.InternetGatewayId' --output text)
+igw_id=$(aws ec2 create-internet-gateway --region $region --query 'InternetGateway.InternetGatewayId' --output text)
 aws ec2 create-tags --resources $igw_id --tags Key=Name,Value=MyInternetGateway
 
 # Attach internet gateway to VPC
-aws ec2 attach-internet-gateway --vpc-id $vpc_id --internet-gateway-id $igw_id
+aws ec2 attach-internet-gateway --region $region --vpc-id $vpc_id --internet-gateway-id $igw_id
 
 # Create a subnet
 echo "Creating Subnet..."
-subnet_id=$(aws ec2 create-subnet --vpc-id $vpc_id --cidr-block 10.0.0.0/24 --query 'Subnet.SubnetId' --output text)
+subnet_id=$(aws ec2 create-subnet --region $region --vpc-id $vpc_id --cidr-block 10.0.0.0/24 --query 'Subnet.SubnetId' --output text)
 aws ec2 create-tags --resources $subnet_id --tags Key=Name,Value=MySubnet
 
 # Create a route table
 echo "Creating Route Table..."
-rtb_id=$(aws ec2 create-route-table --vpc-id $vpc_id --query 'RouteTable.RouteTableId' --output text)
+rtb_id=$(aws ec2 create-route-table --region $region --vpc-id $vpc_id --query 'RouteTable.RouteTableId' --output text)
 aws ec2 create-tags --resources $rtb_id --tags Key=Name,Value=MyRouteTable
 
 # Associate the route table with the subnet
-aws ec2 associate-route-table --subnet-id $subnet_id --route-table-id $rtb_id
+aws ec2 associate-route-table --region $region --subnet-id $subnet_id --route-table-id $rtb_id
 
 # Create a route to internet gateway
-aws ec2 create-route --route-table-id $rtb_id --destination-cidr-block 0.0.0.0/0 --gateway-id $igw_id
+aws ec2 create-route --region $region --route-table-id $rtb_id --destination-cidr-block 0.0.0.0/0 --gateway-id $igw_id
 
 # Create a security group
 echo "Creating Security Group..."
-security_group_id=$(aws ec2 create-security-group --group-name MySecurityGroup --description "My Security Group" --vpc-id $vpc_id --query 'GroupId' --output text)
+security_group_id=$(aws ec2 create-security-group --region $region --group-name MySecurityGroup --description "My Security Group" --vpc-id $vpc_id --query 'GroupId' --output text)
 
 # Authorize inbound SSH traffic in the security group
-aws ec2 authorize-security-group-ingress --group-id $security_group_id --protocol tcp --port 22 --cidr 0.0.0.0/0
+aws ec2 authorize-security-group-ingress --region $region --group-id $security_group_id --protocol tcp --port 22 --cidr 0.0.0.0/0
 
 # Generate a key pair
 echo "Generating Key Pair..."
-aws ec2 create-key-pair --key-name ec2Key --query 'KeyMaterial' --output text > ec2Key.pem
+aws ec2 create-key-pair --region $region --key-name ec2Key --query 'KeyMaterial' --output text > ec2Key.pem
 chmod 400 ec2Key.pem
 
 # EC2 instance
 echo "Launching EC2 instance..."
 aws ec2 run-instances \
+  --region $region \
   --image-id ami-0fc5d935ebf8bc3bc \
   --instance-type t2.micro \
   --key-name ec2Key \
@@ -52,4 +56,3 @@ aws ec2 run-instances \
   --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=MyEC2Instance}]'
 
 echo "Completed!"
-
--- clean_job.sh
+++ clean_job.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
-  echo "Cleaning up resources..."
 
-  # Terminate EC2 instances
-  aws ec2 terminate-instances --instance-ids $(aws ec2 describe-instances --filters "Name=tag:Name,Values=MyEC2Instance" --query 'Reservations[].Instances[].InstanceId' --output text)
+echo "Cleaning up resources in us-west-1 region..."
 
-  # Wait for instances to terminate
-  aws ec2 wait instance-terminated --instance-ids $(aws ec2 describe-instances --filters "Name=tag:Name,Values=MyEC2Instance" --query 'Reservations[].Instances[].InstanceId' --output text)
+# Terminate EC2 instances
+aws ec2 terminate-instances --region us-west-1 --instance-ids $(aws ec2 describe-instances --region us-west-1 --filters "Name=tag:Name,Values=MyEC2Instance" --query 'Reservations[].Instances[].InstanceId' --output text)
 
-  # Disassociate and delete route table
-  aws ec2 disassociate-route-table --association-id $(aws ec2 describe-route-tables --filters "Name=tag:Name,Values=MyRouteTable" --query 'RouteTables[].Associations[].RouteTableAssociationId' --output text)
-  aws ec2 delete-route-table --route-table-id $(aws ec2 describe-route-tables --filters "Name=tag:Name,Values=MyRouteTable" --query 'RouteTables[].RouteTableId' --output text)
+# Wait for instances to terminate
+aws ec2 wait instance-terminated --region us-west-1 --instance-ids $(aws ec2 describe-instances --region us-west-1 --filters "Name=tag:Name,Values=MyEC2Instance" --query 'Reservations[].Instances[].InstanceId' --output text)
 
-  # Detach and delete internet gateway (if exists)
-  igw_id=$(aws ec2 describe-internet-gateways --filters "Name=tag:Name,Values=MyInternetGateway" --query 'InternetGateways[].InternetGatewayId' --output text)
-  if [ -n "$igw_id" ]; then
-    aws ec2 detach-internet-gateway --internet-gateway-id $igw_id --vpc-id $vpc_id
-    aws ec2 delete-internet-gateway --internet-gateway-id $igw_id
-  fi
+# Disassociate and delete route table
+aws ec2 disassociate-route-table --region us-west-1 --association-id $(aws ec2 describe-route-tables --region us-west-1 --filters "Name=tag:Name,Values=MyRouteTable" --query 'RouteTables[].Associations[].RouteTableAssociationId' --output text)
+aws ec2 delete-route-table --region us-west-1 --route-table-id $(aws ec2 describe-route-tables --region us-west-1 --filters "Name=tag:Name,Values=MyRouteTable" --query 'RouteTables[].RouteTableId' --output text)
 
-  # delete  security group ingress rules and delete security group (if exists)
-  security_group_id=$(aws ec2 describe-security-groups --filters "Name=tag:Name,Values=MySecurityGroup" --query 'SecurityGroups[].GroupId' --output text)
-  if [ -n "$security_group_id" ]; then
-    aws ec2 revoke-security-group-ingress --group-id $security_group_id --protocol tcp --port 22 --cidr 0.0.0.0/0
-    aws ec2 delete-security-group --group-id $security_group_id
-  fi
+# Detach and delete internet gateway (if exists)
+igw_id=$(aws ec2 describe-internet-gateways --region us-west-1 --filters "Name=tag:Name,Values=MyInternetGateway" --query 'InternetGateways[].InternetGatewayId' --output text)
+if [ -n "$igw_id" ]; then
+  aws ec2 detach-internet-gateway --region us-west-1 --internet-gateway-id $igw_id --vpc-id $vpc_id
+  aws ec2 delete-internet-gateway --region us-west-1 --internet-gateway-id $igw_id
+fi
 
-  # Delete subnet
-  aws ec2 delete-subnet --subnet-id $(aws ec2 describe-subnets --filters "Name=tag:Name,Values=MySubnet" --query 'Subnets[].SubnetId' --output text)
+# delete security group ingress rules and delete security group (if exists)
+security_group_id=$(aws ec2 describe-security-groups --region us-west-1 --filters "Name=tag:Name,Values=MySecurityGroup" --query 'SecurityGroups[].GroupId' --output text)
+if [ -n "$security_group_id" ]; then
+  aws ec2 revoke-security-group-ingress --region us-west-1 --group-id $security_group_id --protocol tcp --port 22 --cidr 0.0.0.0/0
+  aws ec2 delete-security-group --region us-west-1 --group-id $security_group_id
+fi
 
-  # Delete VPC
-  aws ec2 delete-vpc --vpc-id $vpc_id
+# Delete subnet
+aws ec2 delete-subnet --region us-west-1 --subnet-id $(aws ec2 describe-subnets --region us-west-1 --filters "Name=tag:Name,Values=MySubnet" --query 'Subnets[].SubnetId' --output text)
 
-  echo "Clean-up completed!"
+# Delete VPC
+aws ec2 delete-vpc --region us-west-1 --vpc-id $vpc_id
 
+echo "Clean-up completed!"
